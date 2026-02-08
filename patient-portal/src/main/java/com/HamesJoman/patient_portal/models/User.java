932c33747d59ae61a2b3b6625b936288bae8b0c7
Collin F <thedrunkninja98@gmail.com>
2026-02-08 15:44:16 -0500

    feat: Created User.java

---
+++ patient-portal/src/main/java/com/HamesJoman/patient_portal/models/User.java
@@ -0,0 +1,94 @@
+package com.HamesJoman.patient_portal.models;
+
+import java.time.LocalDateTime;
+
+/**
+ * Model for User.
+ *
+ * @author Collin Fair
+ */
+public abstract class User {
+
+    private int id; // This should probably become long
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password; // hashed string (idk how we're gonna hash yet, look into jwt integration)
+    private String role; // Slight change from how we did it in 427, ask me for clarification if curious
+    private LocalDateTime lastLogin;
+    private LocalDateTime lastPasswordChange;
+
+    public User(int id, String firstName, String lastName, String username, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.lastPasswordChange = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        this.lastPasswordChange = LocalDateTime.now();
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public LocalDateTime getLastPasswordChange() {
+        return lastPasswordChange;
+    }
+
+    public void setLastPasswordChange(LocalDateTime lastPasswordChange) {
+        this.lastPasswordChange = lastPasswordChange;
+    }
+}
